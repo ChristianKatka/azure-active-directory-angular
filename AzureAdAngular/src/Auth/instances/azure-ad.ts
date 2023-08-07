d3853a27422d355f2c782370a7379606e7d05c23
ChristianKatka <christian.katka@skillwell.fi>
2023-08-07 10:13:50 +0300

    easier to understand instance modification

--- AzureAdAngular/src/Auth/instances/azure-ad.ts
+++ AzureAdAngular/src/Auth/instances/azure-ad.ts
@@ -6,9 +6,9 @@ import {
 export const MSALInstance = (): IPublicClientApplication => {
   return new PublicClientApplication({
     auth: {
-      clientId: '<Enter_The_Application_Id_here>',
+      clientId: '<Enter_The_Application_client_ID_HERE>',
       authority:
-        'https://login.microsoftonline.com/<Enter_The_Tenant_Info_Here>',
+        'https://login.microsoftonline.com/<Enter_The_Directory_tenant_ID_HERE>',
       redirectUri: 'http://localhost:4200',
       postLogoutRedirectUri: 'http://localhost:4200',
     },
