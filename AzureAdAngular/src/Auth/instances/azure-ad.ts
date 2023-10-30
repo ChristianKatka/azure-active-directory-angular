ec407637b86a51d55f8a01075498ece63536fb1a
ChristianKatka <42738047+ChristianKatka@users.noreply.github.com>
2023-10-30 11:35:12 +0200

    Update azure-ad.ts

--- AzureAdAngular/src/Auth/instances/azure-ad.ts
+++ AzureAdAngular/src/Auth/instances/azure-ad.ts
@@ -9,8 +9,8 @@ export const MSALInstance = (): IPublicClientApplication => {
       clientId: '<Enter_The_Application_client_ID_HERE>',
       authority:
         'https://login.microsoftonline.com/<Enter_The_Directory_tenant_ID_HERE>',
-      redirectUri: 'http://localhost:4200',
-      postLogoutRedirectUri: 'http://localhost:4200',
+      redirectUri: '/',
+      postLogoutRedirectUri: '/',
     },
   });
 };
